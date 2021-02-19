589eac75d8e994d72595c6d0c95ebef9163e9f3c
Ilya Baskakov <baskakov.11111@mail.ru>
2021-02-19 19:10:06 +0300

    created header

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,25 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Header></Header>
+    <About></About>
+    <Contacts></Contacts>
+    <Footer></Footer>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Header from './components/Header.vue'
+import About from './components/About.vue'
+import Contacts from './components/Contacts.vue'
+import Footer from './components/Footer.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    Header,
+    About,
+    Contacts,
+    Footer,
+  },
 }
 </script>
-
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="header">
+    <div class="header__nav">
+      <div class="header__nav-name">Thrivetalk</div>
+      <ul class="header__nav-list">
+        <li class="header__nav-list-li header__nav-list-li--active">Home</li>
+        <li class="header__nav-list-li">About</li>
+        <li class="header__nav-list-li">Services</li>
+        <li class="header__nav-list-li">Blog</li>
+      </ul>
+      <div class="header__nav-btn">Contact Us</div>
+    </div>
+    <div class="header__content">
+      <div class="header__desc">
+        <div class="header__desc-name">Thrivetalk</div>
+        <div class="header__desc-title">
+          HELPING YOU THRIVE IN ALL AREAS OF LIFE
+        </div>
+        <div class="header__desc-content">
+          Our highly talented therapists can help you with a range of issues
+          including relationships, sex, PTSD, depression, social anxiety, or
+          even just caring for yourself more.
+        </div>
+        <div class="header__desc-recs">
+          <div class="header__desc-recs-active">WHO AM I</div>
+          <div class="header__desc-recs-disabled">WHAT DO I DO</div>
+        </div>
+      </div>
+      <div class="header__img"><img src="@/assets/header-img.png" /></div>
+    </div>
+  </div>
+</template>
