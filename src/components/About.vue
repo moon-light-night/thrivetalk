b306d0dfe0141b425b7887e46cfeac627d3e51a1
Ilya Baskakov <baskakov.11111@mail.ru>
2021-02-20 00:06:35 +0300

    created about-block

---
+++ src/components/About.vue
@@ -0,0 +1,149 @@
+<template>
+  <div class="about">
+    <div class="about__header">
+      <div class="about__header-info">
+        <div class="about__header-name">ABOUT US</div>
+        <div class="about__header-desc">
+          We want to help you thrive! Whether you are just looking for someone
+          to talk to, or are struggling with a mental wellness issue we’re here
+          to help. Our highly talented therapists can help you with a range of
+          issues including relationships, sex, PTSD, depression, social anxiety,
+          or even just caring for yourself more.
+        </div>
+      </div>
+      <p class="about__header-text">WE CAN HELP YOU WITH</p>
+      <div class="about__header-cards">
+        <div class="about__header-card pink">
+          <ul class="about__header-list">
+            <li class="about__header-list-li--empty"></li>
+            <li class="about__header-list-li">Weight Lifting</li>
+          </ul>
+        </div>
+
+        <div class="about__header-card green">
+          <ul class="about__header-list">
+            <li class="about__header-list-li--empty"></li>
+            <li class="about__header-list-li">Running & Spinning</li>
+          </ul>
+        </div>
+
+        <div class="about__header-card blue">
+          <ul class="about__header-list">
+            <li class="about__header-list-li--empty"></li>
+            <li class="about__header-list-li">Pumping Iron</li>
+          </ul>
+        </div>
+
+        <div class="about__header-card dark-blue">
+          <ul class="about__header-list">
+            <li class="about__header-list-li--empty"></li>
+            <li class="about__header-list-li">Pumping Iron</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+    <div class="about__cards">
+      <div class="about__cards-first-row">
+        <div class="about__card deep-blue">
+          <ul class="about__card-list">
+            <li class="about__card-list-li--empty"></li>
+            <li class="about__card-list-li">Physical Health</li>
+          </ul>
+        </div>
+        <div class="about__card dark-blue">
+          <ul class="about__card-list">
+            <li class="about__card-list-li--empty"></li>
+            <li class="about__card-list-li">Mental Health</li>
+          </ul>
+        </div>
+        <div class="about__card straw-img">
+          <ul class="about__card-list">
+            <li class="about__card-list-li--empty"></li>
+            <li class="about__card-list-li">Nutrition</li>
+          </ul>
+        </div>
+      </div>
+      <div class="about__cards-second-row">
+        <div class="about__card blue">
+          <ul class="about__card-list">
+            <li class="about__card-list-li--empty"></li>
+            <li class="about__card-list-li">Gymnastics</li>
+          </ul>
+        </div>
+        <div class="about__card gray">
+          <ul class="about__card-list">
+            <li class="about__card-list-li--empty"></li>
+            <li class="about__card-list-li">Crossfit</li>
+          </ul>
+        </div>
+        <div class="about__card purple">
+          <ul class="about__card-list">
+            <li class="about__card-list-li--empty"></li>
+            <li class="about__card-list-li">Aerobics</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+    <div class="about__descs">
+      <div class="about__descs-first-row">
+        <div class="about__desc-group">
+          <div class="about__desc-title">Nutritional Plans</div>
+          <div class="about__desc-text">
+            Nutritional Facts on some shitty things that you don’t care about
+            and even we don’t , I need to replace this copy.
+          </div>
+        </div>
+        <div class="about__desc-group">
+          <div class="about__desc-title">Weight Loss</div>
+          <div class="about__desc-text">
+            Nutritional Facts on some shitty things that you don’t care about
+            and even we don’t , I need to replace this copy.
+          </div>
+        </div>
+        <div class="about__desc-group">
+          <div class="about__desc-title">Mental Peace</div>
+          <div class="about__desc-text">
+            Nutritional Facts on some shitty things that you don’t care about
+            and even we don’t , I need to replace this copy.
+          </div>
+        </div>
+      </div>
+      <div class="about__descs-second-row">
+        <div class="about__desc-group">
+          <div class="about__desc-title">Home Training</div>
+          <div class="about__desc-text">
+            Nutritional Facts on some shitty things that you don’t care about
+            and even we don’t , I need to replace this copy.
+          </div>
+        </div>
+        <div class="about__desc-group">
+          <div class="about__desc-title">Work/Life Balance</div>
+          <div class="about__desc-text">
+            Nutritional Facts on some shitty things that you don’t care about
+            and even we don’t , I need to replace this copy.
+          </div>
+        </div>
+        <div class="about__desc-group">
+          <div class="about__desc-title">Cardio</div>
+          <div class="about__desc-text">
+            Nutritional Facts on some shitty things that you don’t care about
+            and even we don’t , I need to replace this copy.
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="about__fact">
+      <div class="about__fact-img"><img src="@/assets/linkedin.png" /></div>
+      <div class="about__fact-content">
+        <div class="about__fact-header">YOU SHOULD ALSO KNOW</div>
+        <div class="about__fact-body">
+          MDD affects more than 16.1 million American adults, or about 6.7%of
+          the U.S. population age 18 and older in a given year. We at
+          <span>ThriveTalk</span>
+          can help you.
+        </div>
+        <div class="about__fact-footer">GET HELP NOW</div>
+      </div>
+    </div>
+  </div>
+</template>
