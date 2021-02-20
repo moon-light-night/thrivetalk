e4a120e0dd336c878069a8a563c4e7fcc72c19bb
Ilya Baskakov <baskakov.11111@mail.ru>
2021-02-20 11:43:46 +0300

    created all page

---
+++ src/components/Contacts.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="contacts">
+    <div class="contacts__header">
+      <div class="contacts__header-body deep-blue">
+        <div class="contacts__header-uptitle">CONTACT US</div>
+        <div class="contacts__header-title">Ready. Set. Smile</div>
+        <div class="contacts__header-text">
+          Take the free online assessment to see if you are a candidate and get
+          started on your journey.
+        </div>
+        <div class="contacts__header-footer">GET HELP NOW</div>
+      </div>
+      <div class="contacts__header-img-wrap">
+        <img
+          src="@/assets/charles.png"
+          alt="img"
+          class="contacts__header-img"
+        />
+      </div>
+    </div>
+    <div class="contacts__footer">
+      <div class="contacts__footer-img-wrap">
+        <img src="@/assets/smile.png" alt="img" class="contacts__footer-img" />
+      </div>
+      <div class="contacts__footer-content">
+        <div class="contacts__footer-desc">
+          <div class="contacts__footer-title">SOME INFO</div>
+          <div class="contacts__footer-text">
+            Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean
+            commodo ligula.
+          </div>
+        </div>
+        <div class="contacts__footer-desc">
+          <div
+            class="contacts__footer-title
+          "
+          >
+            ENFOLD HEALTH
+          </div>
+          <div class="contacts__footer-text">
+            Main Street 1, Olcott Buffalo, United States<br /><br />
+            +555 283 784 333 office@enfold-health.com
+          </div>
+        </div>
+        <div class="contacts__footer-desc">
+          <div class="contacts__footer-title">OFFICE HOURS</div>
+          <div class="contacts__footer-text">
+            Mo-Fr: 8:00-19:00 Sa: 8:00-14:00 Su: closed
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
---
+++ src/components/Footer.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="footer blue">
+    <div class="footer__copyright">
+      <div class="footer__copyright-item">
+        <img src="@/assets/copy.png" alt="img" class="footer__copyright-img" />
+      </div>
+      <div class="footer__copyright-item">Copyright - ThriveTalk 2017</div>
+    </div>
+  </div>
+</template>
